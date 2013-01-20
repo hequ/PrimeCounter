b3437d0de91cffc02b24e21fe00823d26c6eba0c
Henri Kinnunen <hequmania@gmail.com>
2013-01-20 14:06:40 +0200

    remove comments. reformat description

--- src/main/java/aatkin/prime/Primes.java
+++ src/main/java/aatkin/prime/Primes.java
@@ -1,11 +1,9 @@
 package aatkin.prime;
 
-import java.math.*;
-
-
-// Author: Anssi Kinnunen, aatkin@utu.fi
-//
-// A small program for calculating prime numbers from natural numbers in a given range.
+/**
+ *  Author: Anssi Kinnunen, aatkin@utu.fi
+ *  A small program for calculating prime numbers from natural numbers in a given range.
+ */
 
 public class Primes {
     public static void main(String[] args) {
@@ -21,18 +19,15 @@ public class Primes {
     }
 
     public static void calculatePrimes(int limitFrom, int limitTo) {
-        // if boundaries are broken, abort
         if (limitTo < limitFrom) {
             System.out.println("Limits out of boundary: limitFrom (" + limitFrom + ") > limitTo (" + limitTo + ")");
             System.exit(0);
         }
-        // if boundaries are broken, abort
         else if (limitFrom < 1 || limitTo < 1) {
             System.out.println("Limits must be positive and non-zero! Limits: " + limitFrom + " " + limitTo);
             System.exit(0);
         }
 
-        //check every number beginning from limitFrom to limitTo whether they pass the isPrime(...) check.
         for (int i = limitFrom; i <= limitTo; i++) {
             if (isPrime(i)) {
                 System.out.println(i);
@@ -42,12 +37,8 @@ public class Primes {
 
     public static boolean isPrime(double number) {
         if (number == 1) return true;
-        // limit for checking (current number) modus (int i) is sqrt of the number
-        // because [insert good explanation why it's just so]
         double limit = Math.sqrt(number);
 
-        // check all numbers starting from 2 to the limit
-        // if given number modus integer i equals zero, number is not a prime, return false
         for (int i = 2; i <= limit; i++) {
             if ((int) number % i == 0) {
                 return false;
@@ -56,4 +47,4 @@ public class Primes {
         return true;
     }
 
-} //PrimeTest-class
+}
